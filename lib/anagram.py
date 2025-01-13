fa888eac2072758cb21c7e60831b7d1f5a289262
Ruben Salazar <rubensalazar@rocketmail.com>
2025-01-13 13:47:27 -0600

    anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted(word)
+
+    def match(self, possible_anagrams):
+        matches = []
+        for candidate in possible_anagrams:
+            if candidate != "".join(self.word) and sorted(candidate) == self.word:
+                matches.append(candidate)
+        return matches
+
+    
